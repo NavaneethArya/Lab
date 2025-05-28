75f5cc09d8bc94077ef12522806382afaf615e03
navaneeth arya <navaneetharyarao@gmail.com>
2025-05-28 22:50:18 +0530

    Initial commit

---
+++ src/main/java/com/program2/maven/program2_example_jar/App.java
@@ -0,0 +1,13 @@
+package com.program2.maven.program2_example_jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worl!" );
+    }
+}
